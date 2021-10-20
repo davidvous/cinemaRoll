d6b44bea27472983d7862f06dfe396a04ea7c923
davidvous <ledavidvu@gmail.com>
2021-10-20 03:30:21 -0700

    additional divs and styling

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const movieObj = {
   genre_ids: [2,3,7],
-  overview: "Luca and his best friends",
+  overview: "Lebron James",
   poster_path: "https://hs.sbcounty.gov/cn/Photo%20Gallery/_w/Sample%20Picture%20-%20Koala_jpg.jpg",
   release_date: "2021-07-08",
   title: "Space Jam",
@@ -13,7 +13,7 @@ const movieObj = {
 }
 
 router.get("/", function (req, res, next) {
-  res.render('movies', { movieObj: movieObj } )
+  res.render('movies', { movieObj } )
 });
 
 module.exports = router;
