e59489feb1ea438e675d4c8142f0e94b0d72c613
davidvous <ledavidvu@gmail.com>
2021-10-20 14:52:04 -0700

    added movies:id route

--- routes/movies.js
+++ routes/movies.js
@@ -3,17 +3,6 @@ const { asyncHandler } = require("./utils");
 const db = require("../db/models");
 const router = express.Router();
 
-const movieObj = {
-  genre_ids: [2, 3, 7],
-  overview:
-    "Eget egestas purus viverra accumsan. Lorem ipsum dolor sit amet consectetur adipiscing elit. In massa tempor nec feugiat nisl pretium. Et leo duis ut diam quam nulla porttitor massa. Consectetur libero id faucibus nisl tincidunt eget nullam non. Arcu cursus euismod quis viverra nibh cras pulvinar. Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Arcu ac tortor dignissim convallis aenean et. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Amet tellus cras adipiscing enim eu turpis egestas. Augue mauris augue neque gravida in fermentum et. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Vitae ultricies leo integer malesuada nunc. Vivamus at augue eget arcu. Turpis massa sed elementum tempus.",
-  poster_path:
-    "https://hs.sbcounty.gov/cn/Photo%20Gallery/_w/Sample%20Picture%20-%20Koala_jpg.jpg",
-  release_date: "2021-07-08",
-  title: "Space Jam",
-  id: 38,
-};
-
 //reviews table will need reviewDate
 const reviewObj = {
   title: "The movie was great.",
@@ -24,8 +13,17 @@ const reviewObj = {
   reviewDate: "2021-10-20",
 };
 
-router.get("/", function (req, res, next) {
-  res.render('movies', { movieObj, reviewObj } )
-});
+// should route to genres to display "all movies"
+router.get("/", asyncHandler(async (req, res) => {
+  res.render("listGenres");
+}));
+
+router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
+  const movieId = parseInt(req.params.id);
+  const movie = await db.Movie.findByPk(movieId);
+  const ratingDecimal = (movie.popularity / 1000).toFixed(2);
+  res.render("movies", { movieObj: movie, ratingDecimal, reviewObj });
+}));
+
 
 module.exports = router;
