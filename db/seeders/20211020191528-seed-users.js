08cb0ba3fce59cf8248629589401533a2d13a9da
jas0123uah <jas0123@uah.edu>
2021-10-20 17:23:24 -0400

    Seeder data.

--- db/seeders/20211020191528-seed-users.js
+++ db/seeders/20211020191528-seed-users.js
@@ -2,16 +2,33 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      return queryInterface.bulkInsert('People', [
+        {id:1,
+        email:"jim@jim.com",
+        passwordHash:"$2a$10$tuOOz8wLRds1Z03iGlNEr.ErjYfFCivNYHfVb33ulaGrRqtxnaDsa",
+        firstName: "Jim",
+      lastName:"Regan",
+    },
+    {id:2,
+        email:"dave@dave.com",
+        passwordHash:"$2a$10$JehxAO3xJHfj6HcdvSjjXObkYAOhkFQIH2IePH8ruHiLxQSjBMOU2",
+        firstName:"Dave",
+      lastName:"Davidson",
+    },
+    {id:3,
+        email:"linda@linda.com",
+        passwordHash:"$2a$10$SS.RZXG1dMtEhVcdsabg7uHbG6fVOB9L4vMTXAlvXHBXeCZNvZC8S",
+        firstName:"Linda",
+      lastName:"White",
+    },
+    {id:4,
+        email:"bob@bob.com",
+        passwordHash:"$2a$10$47B5GHg4cNGp1YpfdUELxOxpZvOH3S9rk04zcAJSU.hwL5pgkSjCu",
+        firstName:"Bob",
+      lastName:"Jones",
+    },
+    ], {});
+  
   },
 
   down: (queryInterface, Sequelize) => {
