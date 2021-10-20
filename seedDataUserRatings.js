2c301350f7cc25b3b0dc6ca062ba12e98512a2ab
davidvous <ledavidvu@gmail.com>
2021-10-20 14:15:53 -0700

    refactored seedUserRating

--- seedDataUserRatings.js
+++ seedDataUserRatings.js
@@ -1,7 +1,6 @@
 var fs = require("fs");
 const movies = require('./db/tmdb_movies_filtered.json')
 
-let props = ["createdAt", "updatedAt", "dateReleased", "summary", "posterPath", "adult", "backdrop_path", "genre_ids", "original_language", "video", "original_title", "overview", "popularity", "poster_path", "release_date", "title", "video", "vote_average", "vote_count"]
 let filteredMovies = [];
 
 addUserRatings = (array) => {
@@ -9,12 +8,9 @@ addUserRatings = (array) => {
     for (let i = 0; i < array.length; i++) {
         const movie = array[i];
         for (let key in movie) {
-            for (j = 0; j < props.length; j++) {
-                let indivProp = props[j];
-                if (movie.hasOwnProperty(indivProp)) {
-                    delete movie[indivProp];
+                if (key !== 'id') {
+                    delete movie[key];
                 }
-            }
             movie["userId"] = Math.floor(Math.random() * (4) + 1);
             movie["rating"] = Math.floor(Math.random() * 5 + 1);
             
@@ -25,4 +21,4 @@ addUserRatings = (array) => {
 }
 
 addUserRatings(movies)
-fs.writeFileSync('/Users/davidle/Desktop/AppAcademy/cinemaRoll/db/userRatingsFiltered.json', JSON.stringify(movies));
+fs.writeFileSync('./db/userRatingsFiltered.json', JSON.stringify(movies));
