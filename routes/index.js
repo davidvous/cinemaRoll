9dcd38a25111ab700072d5619837d9f2680b80e2
ily123 <ibnovikov@gmail.com>
2021-10-19 23:00:59 -0500

    Cleaned up genre block.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  res.render('index', {
+    genres: [
+      ['genre1', 'genre2', 'genre3', 'genre4', 'genre5'],
+      ['genre1', 'genre2', 'genre3', 'genre4', 'genre5'],
+      ['genre1', 'genre2', 'genre3', 'genre4', 'genre5'],
+      ['genre1', 'genre2', 'genre3']
+    ]
+  });
 });
 
 module.exports = router;
