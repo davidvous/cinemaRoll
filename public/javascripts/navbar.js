588c0e9c835fce06ea2820aa374536bd0f0adbb8
ily123 <ibnovikov@gmail.com>
2021-10-20 01:50:26 -0500

    Removed alerts from navbar js.

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', event => {
-
   const dropDown = () => {
     document.getElementById("dropDownLink").classList.toggle("show");
   }
 
   window.onclick = function(event) {
-    //alert("test");
     if (!event.target.matches('.dropDownLink')) {
       let dropdowns = document.getElementsByClassName("dropdown-content");
       for (let i = 0; i < dropdowns.length; i++) {
