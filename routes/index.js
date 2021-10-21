c619b8c6c12fc426c2dc087121c4a18093b1cd9e
ily123 <ibnovikov@gmail.com>
2021-10-21 11:56:43 -0500

    Added movie posters & links rendering to index view.

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,18 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res) => {
-  // the template expects sub-arrays of size 5
+  // the template expects genres in sub-arrays of size 5
   const genres_ = await db.Genre.findAll().map(g => g.dataValues);
   const genres = [];
   while(genres_.length) genres.push(genres_.splice(0, 5));
-  res.render('index', { genres } );
+
+  // the template expects top 10 movies
+  const topMovies = await db.Movie.findAll({
+    limit: 10,
+    order: [["popularity", "DESC"]]
+  }).map(m => m.dataValues);
+
+  res.render('index', { genres, topMovies } );
 }));
 
 module.exports = router;
