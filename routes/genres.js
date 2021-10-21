cfa5e431f44ee937e329d080f4fb9eca12e659fb
davidvous <ledavidvu@gmail.com>
2021-10-21 08:13:55 -0700

    styling genres and dynamic

--- routes/genres.js
+++ routes/genres.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {asyncHandler} = require('./utils')
 const db = require('../db/models');
-//console.log(db)
 const router = express.Router();
+
 router.get('/', asyncHandler( async (req, res, next) => {
   const actionMovies = await db.Movie.findAll({ order: [db.Sequelize.fn('RANDOM')], limit: 5,
-include: ['Genres']})
+    include: ['Genres']});
 
   // const comedyMovies = await db.Movie.findAll({ order: Sequelize.literal('rand()'), limit: 5,
   // where: { genreId: 4} })
@@ -17,14 +17,13 @@ include: ['Genres']})
   // where: { genreId: 14} })
 
   //res.render("temp", {actionMovies, comedyMovies, crimeMovies, romanceMovies}) 
-  console.log(actionMovies[0].title, actionMovies[0].Genres)
-  //console.log(actionMovies.Genres)
 
-  res.render("listGenres", {actionMovies})  
+  console.log(actionMovies);
+  res.render("listGenres", {movies: actionMovies});  
 }));
 
 router.get('/:genre([A-Za-z]+)', asyncHandler( async (req, res) => {
-  console.log(req.params, "<---- These are our params");
+
   const reqGenre = req.params.genre
   let reqsGenreId = await db.Genre.findOne({
     where: {
@@ -33,18 +32,17 @@ router.get('/:genre([A-Za-z]+)', asyncHandler( async (req, res) => {
   })
   
 
-  console.log(reqsGenreId.id)
+  // console.log(reqsGenreId.id)
   reqsGenreId = reqsGenreId
 
   const movies = await db.Movie.findAll({ order: [db.Sequelize.fn('RANDOM')], limit: 4,
-include: ['Genres']})
+  include: ['Genres']})
 
 // const actionMovies = await db.genresToMovieJoinTable.findAll({ order: [db.Sequelize.fn('RANDOM')], limit: 5,
 // where:{
 //   genreId: reqsGenreId 
 // },
 // include: ['Movies'] })
-console.log(movies)
 res.render('listGenres', {movies} );
 //   const moviesFromRequestedGenre = await db.Movies.findAll({ order: [db.Sequelize.fn('RANDOM')], limit: 5,
 //   where:  {
