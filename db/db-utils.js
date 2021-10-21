982569076291be7d0d14f5d841a5c5cb946e4bbd
ily123 <ibnovikov@gmail.com>
2021-10-21 00:25:12 -0500

    Added movie export to db-utils that will seed into the db.

--- db/db-utils.js
+++ db/db-utils.js
@@ -6,68 +6,67 @@ const createdAt = new Date().toISOString();
 const updatedAt = createdAt;
 
 
-const genreNames = genresTMDB.map(genre => {
+// remove overly gratuitous movies
+const isBanned = (id) => {
+  const banList = new Set([
+    719088, 81774, 440249, 381749, 337167, 441168, 873491, 197158,
+    20222, 61927, 84354, 766680, 818659, 351819, 360789, 451156,
+    24593, 487297, 106758
+  ]);
+  return banList.has(id);
+}
+
+let movies = moviesTMDB.filter(movie => !isBanned(movie.id));
+
+
+// Create genres object for seeding into the database
+// give them ids 1 through L.
+//
+// Createa a map of old genres ids vs new ids
+let newId = 0;
+const idMapper = {}
+const genres = genresTMDB.map(genre => {
+  newId += 1
   const name = genre.name;
+  idMapper[genre.id] = newId;
   return {
+    id: newId,
     name,
     createdAt,
     updatedAt
   };
 });
 
-
-// there are duplicate movie titles
-// our db does not allow for duplicates
-// add TMDB movie id to title to resolve the validation error for now
-// this is a temporary fix ("temporary" hehe)
+// Create movie array to be seeded into the database
+// - assign ids starting with 1
+// - add movie id to title to resolve unique title validation error
+// - replace missing values
 const titles = new Set();
+let newMovieId = 0;
+movies = movies.map(movie => {
+  newMovieId += 1;
 
-const movies = moviesTMDB.map(movie => {
-
-  let { genre_ids, original_title, overview, release_date, id} = movie;
-  if (titles.has(original_title)) original_title += id;
+  let { genre_ids, original_title, overview, release_date, popularity, poster_path} = movie;
+  if (titles.has(original_title)) original_title += newMovieId;
   if (!overview) overview = "The summary for this movie was not provided";
   if (!release_date) release_date = "09-09-9999";
   titles.add(original_title);
 
   return {
+    "id": newMovieId,
     "title": original_title,
     "dateReleased": release_date,
     "summary": overview,
+    popularity,
     createdAt,
-    updatedAt
+    updatedAt,
+    posterPath: "https://image.tmdb.org/t/p/original" + poster_path
   }
 
 });
 
 
-// overly gratuitous movies to be removed from the set before seeding
-const banList = [
-  719088,
-  81774,
-  440249,
-  381749,
-  337167,
-  441168,
-  873491,
-  197158,
-  20222,
-  61927,
-  84354,
-  766680,
-  818659,
-  351819,
-  360789,
-  451156,
-  24593,
-  487297,
-  106758
-]
-
-
 module.exports = {
-  genreNames,
+  genres,
   movies,
-  banList
 }
-
