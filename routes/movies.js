883c9742b2919c6a52707575abc4140a745fd79b
jas0123uah <jas0123@uah.edu>
2021-10-21 22:46:06 -0400

    Added express validators. Moved pug fles I used in testing to main views directory to allow for easier testing.

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const { asyncHandler, csurfProtection } = require("./utils");
+const { check, validationResult } = require('express-validator');
 const db = require("../db/models");
 const router = express.Router();
+const createError = require('http-errors')
+
+
+const movieValidators = [
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage('Please title for your review.')
+    .isLength({ max: 285 })
+    .withMessage('Review title must not be more than 285 characters long'),
+  check('reviewText')
+    .isLength({min: 10})
+    .withMessage('Review must be at least 10 characters long.'),
+  check('rating')
+    .isInt({min: 1, max: 5})
+    .withMessage('Rating must be between 1 and 5 stars.'),
+];
+
 
 
 // should route to genres to display "all movies"
@@ -9,9 +27,14 @@ router.get("/", asyncHandler(async (req, res) => {
   res.render("listGenres");
 }));
 
-router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
+
+//DONE: GETS MOVIE BY ID
+router.get("/:id(\\d+)", asyncHandler(async (req, res, next) => {
   const movieId = parseInt(req.params.id);
   const movie = await db.Movie.findByPk(movieId);
+  if (!movie) {
+    next(createError(404))
+  }
   const ratingDecimal = (movie.popularity / 1000).toFixed(2);
   const reviews = await db.Review.findAll({
     where: { movieId: movieId }
@@ -24,7 +47,8 @@ router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
 }));
 
 
-router.get('/:id(\\d+)/reviews/', asyncHandler( async (req, res) => {
+// DONE: GETS ALL REVIEWS ASSOCIATED W/MOVIE
+router.get('/:id(\\d+)/reviews/', asyncHandler( async (req, res, next) => {
   //Get all reviews associated w/ a movie
   let allReviewsForMovie = await db.Movie.findAll({include: ['Reviews'],
   where:{
@@ -38,70 +62,95 @@ router.get('/:id(\\d+)/reviews/', asyncHandler( async (req, res) => {
   })
 allReviewsForMovie=allReviewsForMovie[0]
 
-
+if (!allReviewsForMovie) {
+    next(createError(404))
+  }
 const {popularity, dateReleased, title, summary, poster_path, Reviews} = allReviewsForMovie
 
 
-
 res.render("reviewsForMovieWithId", {popularity, dateReleased, title, summary, poster_path, Reviews} )
-console.log(req.session, "<------");
 
 
 }));
 
-router.post('/:id(\\d+)/reviews/', csurfProtection, asyncHandler( async (req, res) => {
+
+//THE POST ROUTE ISNT WORKING? I GET A VALIDATION ERROR. I'M NOT SURE WHY 
+router.post('/:id(\\d+)/reviews/', csurfProtection, movieValidators, asyncHandler( async (req, res) => {
   //Add a new review for a given movie
   if (!req.session.auth) {
     res.redirect("http://localhost:8080/users/login/");
     
   }
-  const movieId = req.params.id
+  const movieId = Number(req.params.id)
   console.log(req.session.auth, "<------");
-  const userId = req.session.auth.userId
-  const movie = await db.Movie.findByPk(movieId);
+  const userId = Number(req.session.auth.userId)
+  //const movie = await db.Movie.findByPk(movieId);
+  //
   const {
     title,
     reviewText,
+    rating
   } = req.body 
 
   const validatorErrors = validationResult(req);
 
     if (validatorErrors.isEmpty()) {
-      await Review.create({title, reviewText, movieId, userId})
-      res.redirect(`/${movieId}/reviews`);
+      console.log(title, reviewText, movieId, userId)
+      await db.Review.create({title, reviewText, movieId, userId})
+    
+      res.redirect(`http://localhost:8080/movies/${movieId}/reviews` );
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
+      console.error(errors)
       res.render('addMovieReview', {
-        title: `Add review for ${movie.title}`,
-        park,
-        attraction,
+        movieId,
+        title,
+        reviewText,
         errors,
+        rating,
         csrfToken: req.csrfToken(),
       });
     }
 
 }));
 
-
+//DONE: Gets form for new movie
 router.get('/:id(\\d+)/reviews/new/', csurfProtection, asyncHandler( async (req, res) => {
   //Get the form to add a review that will be associated w/ a movie
   const movieId = req.params.id;
+  if (!req.session.auth) {
+    res.redirect(`http://localhost:8080/users/login/`)
+  }
   res.render("addMovieReview", {csrfToken: req.csrfToken(), movieId})
 
 }));
 
 
-router.get('/:id(\\d+)/reviews/:reviewId(\\d+)/edit',csurfProtection, asyncHandler( async (req, res) => {
+
+//WORKS, BUT NOT DONE: RENDERS PAGE W/ BUTTON TO DELETE ROUTE. DELETE WORKS. NEED TO ADD EDIT FORM TO TEST PUT/PATCH OF REVIEWS
+router.get('/:id(\\d+)/reviews/:reviewId(\\d+)/edit',csurfProtection, asyncHandler( async (req, res, next) => {
     const movieId = req.params.id;
     const reviewId = req.params.reviewId
+    //console.log(req.params)
+    //console.log(reviewId)
+    
 
-    const review = await db.Review.findByPk(req.params.reviewId)
+    const review = await db.Review.findByPk(reviewId)
+    if (!review) {
+    next(createError(404))
+  }
+  
     const userIdOfMovieReviewer = review.userId
+    //http://localhost:8080/movies/3/reviews/2/edit
+    //userIdOfMovieReviewer
+    //ComeBack Add stuff to get users old review. Prepopulate fields
+
 
     if (req.session.auth && userIdOfMovieReviewer == req.session.auth.userId) {
-      let reviewToEdit = await db.Review.findByPk(userIdOfMovieReviewer);
+      let reviewToEdit = await db.Review.findByPk(req.params.reviewId);
+    
       reviewToEdit=reviewToEdit.dataValues
-      console.log(reviewToEdit)
+      
       res.render("reviewEditForm", {csrfToken: req.csrfToken(), reviewToEdit, movieId, reviewId});
       
       
@@ -109,29 +158,58 @@ router.get('/:id(\\d+)/reviews/:reviewId(\\d+)/edit',csurfProtection, asyncHandl
       res.redirect("http://localhost:8080/users/login")       
     
     }
-  //Get the form to edit a specific movie review
 
 
 }));
 
-router.put('/:id(\\d+)/reviews/:reviewId(\\d+)/edit', csurfProtection, asyncHandler( async (req, res) => {
+//NEED TO DETERMINE CAUSE OF SEQUELIZE VALIDATION ERROR
+router.put('/:id(\\d+)/reviews/:reviewId(\\d+)/edit', csurfProtection, movieValidators, asyncHandler( async (req, res) => {
   //Edit a specific movie review
+
+  const review = await db.Review.findByPk(req.params.reviewId)
+    if (!review) {
+    next(createError(404))
+  }
+    const userIdOfMovieReviewer = review.userId
+    if (userIdOfMovieReviewer == req.session.auth.userId) {
+      //const deletedReview = await db.Review.findByPk(req.params.);
+      // await deletedReview.destroy();
+      await review.update();
+      console.log("destroyed")
+
+      res.redirect(`http://localhost:8080/movies/${movieId}/reviews/`);
+      
+    }else{
+       res.redirect('http://localhost:8080/users/login');
+
+    }
+
+
+  
   
 
 }));
 
 
-
-router.post('/:id(\\d+)/reviews/:reviewId(\\d+)/delete',  asyncHandler(async (req, res) => {
+//DONE: ROUTE DELETES REVIEW
+router.post('/:id(\\d+)/reviews/:reviewId(\\d+)/delete',  asyncHandler(async (req, res, next) => {
+    const movieId = req.params.id
+    console.log("req.params.userId", req.params.userId)
     const review = await db.Review.findByPk(req.params.reviewId)
+    if (!review) {
+    next(createError(404))
+  }
     const userIdOfMovieReviewer = review.userId
     if (userIdOfMovieReviewer == req.session.auth.userId) {
-      const deletedReview = await db.Review.findByPk(userIdOfMovieReviewer);
-      await deletedReview.destroy();
-      res.redirect(`movies/:id(\\d+)/reviews/`);
+      //const deletedReview = await db.Review.findByPk(req.params.);
+      // await deletedReview.destroy();
+      await review.destroy();
+      console.log("destroyed")
+
+      res.redirect(`http://localhost:8080/movies/${movieId}/reviews/`);
       
     }else{
-       res.redirect('/users/login');
+       res.redirect('http://localhost:8080/users/login');
 
     }
 
