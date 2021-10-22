8153ec402e6ac475d1b3a5c6d5d79fd3f1c5ff1f
ily123 <ibnovikov@gmail.com>
2021-10-21 23:43:32 -0500

    Added boilerplate for movie list display.

--- routes/lists.js
+++ routes/lists.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 
-
-const {csurfProtection, asyncHandler} = require('./utils')
 const db = require('../db/models');
 const { loginUser, logoutUser } = require('../auth');
+const {csurfProtection, asyncHandler} = require('./utils')
 
 
 const router = express.Router();
 
-router.get("/", asyncHandler(async (req, res) => {
+
+router.get('/', asyncHandler(async (req, res) => {
+
   const { authenticated, user } = res.locals;
-  res.json({authenticated, user});
+  if (!authenticated || !user) {
+    return res.redirect('/users/login');
+  }
+
+  // the template expects top 10 movies
+  const topMovies = await db.Movie.findAll({
+      limit: 15,
+      order: [["popularity", "DESC"]]
+  }).map(m => m.dataValues);
+  //res.json({authenticated, user});
+  res.render('mymovies', {topMovies});
+
 }));
 
 
