9b3bc1c6845f66da7d3e1c5345447abc898e0719
ily123 <ibnovikov@gmail.com>
2021-10-23 13:58:13 -0500

    Added list poster rendering.

--- public/javascripts/lists.js
+++ public/javascripts/lists.js
@@ -22,10 +22,33 @@ const listRender = async (event) => {
   } else {
     const titles = content.Movies.map(m => m.title).join("\n");
     console.log(titles);
+    renderPosters(content.Movies);
   }
 };
 
 
-const renderPosters = () => {
+const renderPosters = (movies) => {
+  console.log(movies);
+  // create new elements
+  const movieCards = []
+  movies.forEach(movie => {
+    const { id, title, posterPath } = movie;
+    // create movie card components
+    const wrapper    = document.createElement("div");
+    const navigation = document.createElement("a");
+    const image      = document.createElement("img");
+    // populate components with attributes
+    image.src = posterPath;
+    navigation.href = "/movies/" + id
+    // assemble components together
+    navigation.appendChild(image);
+    wrapper.append(navigation);
+    wrapper.className = "movie_wrapper"
+    movieCards.push(wrapper);
+  });
 
+  // delete existing movie cards from the page & insert new ones
+  const movieContainer = document.getElementById("top_movies_grid-4");
+  movieContainer.innerHTML = '';
+  movieCards.forEach(card => movieContainer.appendChild(card));
 }
