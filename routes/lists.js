2ab72f58a77e9ab3464ff65147171cd744206d65
ily123 <ibnovikov@gmail.com>
2021-10-22 21:15:55 -0500

    Fixed js import.

--- routes/lists.js
+++ routes/lists.js
@@ -8,13 +8,9 @@ const {csurfProtection, asyncHandler} = require('./utils')
 
 const router = express.Router();
 
-
-// brrrr, let's sketch out what i need to do
-// GET  '/' - regular HTML request / response that loads all movies in user's list on page
-// POST '/:listName' - creates a new list for the session user with the provided list name
-// -----
-// GET  '/:listName' - ..... DO NOT NEED THIS ONE, we dump all lists when we do 'GET'
 //
+//TODO: middleware to redirect users who are not logged in
+
 
 router.get('/', asyncHandler(async (req, res) => {
 
@@ -23,38 +19,16 @@ router.get('/', asyncHandler(async (req, res) => {
     //return res.redirect('/users/login');
   }
 
-  // the template expects top 10 movies
-  const topMovies = await db.Movie.findAll({
-      limit: 15,
-      order: [["popularity", "DESC"]]
-  }).map(m => m.dataValues);
-
-  const test = await db.MovieList.findAll({
+  const lists = await db.MovieList.findAll({
     where: { userId: 1 },
     include: [{ model: db.Movie }]
-  }).map(m => m.dataValues);
-  console.log(test);
+  })
 
-  res.render('mymovies', {topMovies});
-
+  const movies = []
+  lists.forEach(list => movies.push(...list.Movies));
+  res.render('mymovies', { movies });
 }));
 
-
-
-// To make requests from the browser, do
-// let response = await fetch('/lists', { method: "POST" }
-// let res = await fetch('/lists', {
-//   method: "POST",
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({"xyz": "hello"})
-// });
-// let content  = await response.json()
-//
-//
-// get user ids from res.locals.user
-// req.session.auth = { userId: user.id }
 
 // creates list of movies for user
 router.post('/', asyncHandler(async (req, res) => {
