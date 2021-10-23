ab1c3cbc5a72461008b9eebb3892cfccfc21a77d
ily123 <ibnovikov@gmail.com>
2021-10-23 13:18:25 -0500

    Added event listener that fires off a GET request to list links.

--- public/javascripts/lists.js
+++ public/javascripts/lists.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', async event => {
-  console.log("hello");
+
+  const sideBar = document.getElementById("sidebar");
+  const lists = document.getElementsByClassName("listItem");
+
+  Array.from(lists).forEach(list => {
+    list.addEventListener("click", listRender);
+  });
+  console.log(lists);
+
 });
+
+
+const listRender = async (event) => {
+  // there is gotta be a better way to do this (stick into a form and get action?)
+  const listId = event.target.id.split("-")[1];
+  let res = await fetch('/lists' + "/" + listId);
+  let content = await res.json();
+  if (content.message) {
+    //listDisplay.innerText = content.message;
+    console.log("hello");
+  } else {
+    const titles = content.Movies.map(m => m.title).join("\n");
+    console.log(titles);
+  }
+};
+
+
+const renderPosters = () => {
+
+}
